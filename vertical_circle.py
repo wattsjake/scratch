e9a071036bcdc454008a2e13c48bb7c041245094
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-18 10:33:41 -0700

    added plot

--- vertical_circle.py
+++ vertical_circle.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # equation for the integral of the square root of 1-x^2 dx
 def equation(b,a=0):
@@ -22,7 +23,47 @@ difference = np.abs(output-(area))
 #print the output value for the smallest difference
 print(d[np.argmin(difference)])
 
-#verify the output
+#plot a unit circle and the area of the slice
+plt.plot(np.cos(np.linspace(0,2*np.pi,100)),np.sin(np.linspace(0,2*np.pi,100)))
+plt.plot(np.cos(np.linspace(0,np.pi/2,100)),np.sin(np.linspace(0,np.pi/2,100)))
+plt.plot(np.cos(np.linspace(0,np.pi/2,100)),np.sin(np.linspace(0,np.pi/2,100))*-1)
+plt.plot(np.cos(np.linspace(0,np.pi/2,100))*-1,np.sin(np.linspace(0,np.pi/2,100))*-1)
+plt.plot(np.cos(np.linspace(0,np.pi/2,100))*-1,np.sin(np.linspace(0,np.pi/2,100)))
+plt.plot(np.cos(np.linspace(0,np.pi/2,100)),np.sin(np.linspace(0,np.pi/2,100))*-1)
+plt.plot(np.cos(np.linspace(0,np.pi/2,100))*-1,np.sin(np.linspace(0,np.pi/2,100)))
+plt.plot(np.cos(np.linspace(0,np.pi/2,100))*-1,np.sin(np.linspace(0,np.pi/2,100))*-1)
+plt.plot(np.cos(np.linspace(0,np.pi/2,100)),np.sin(np.linspace(0,np.pi/2,100)))
+
+#plot a vertical line at the value of the smallest difference and mirror it over the y axis and make it red and dashed
+plt.plot([d[np.argmin(difference)],d[np.argmin(difference)]],[-1,1],color='r',linestyle='--')
+
+
+#plot a grid over the plot and place a origin at the center
+plt.grid()
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+
+#change the screen to be square
+plt.axis('square')
+
+
+
+
+
+
+
+
+
+
+plt.show()
+
+#plot the output
+# plt.plot(d,output)
+# plt.plot(d,difference)
+# plt.show()
+
+
+
 
 
 
