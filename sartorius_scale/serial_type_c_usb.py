7c359721a45077af8d5c33e4bb681390131e4539
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-27 12:48:11 -0700

    added sartorius manual for communication

--- sartorius_scale/serial_type_c_usb.py
+++ sartorius_scale/serial_type_c_usb.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import serial
+  
+ser = serial.Serial("COM3", 
+                     115200, 
+                     timeout = 3, 
+                     bytesize=serial.EIGHTBITS, 
+                     parity=serial.PARITY_ODD,
+                     stopbits=serial.STOPBITS_ONE)
 
+#read from display
+data = ser.read(100)
+print(data)
