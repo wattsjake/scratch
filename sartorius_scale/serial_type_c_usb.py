f0d47ebadc2d45667621555f8dc6c7e4d740046b
noahmazza <noah.mazza17@gmail.com>
2023-01-27 13:02:20 -0700

    Documentation and Serial C additions

--- sartorius_scale/serial_type_c_usb.py
+++ sartorius_scale/serial_type_c_usb.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import serial
+
+serial_port = 'COM4'
+baud_rate = 115200
+ser = serial.Serial(serial_port, baud_rate, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
+
+print(ser)
+
+ser.close()
+
+
+
+
