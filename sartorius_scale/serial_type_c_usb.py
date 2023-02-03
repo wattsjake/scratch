ef50a551b5a6db8ceeaa747343e67256943dd4dd
wattsjake <watts.jacob.samuel@gmail.com>
2023-02-03 12:42:30 -0700

    added to the serial_type_c

--- sartorius_scale/serial_type_c_usb.py
+++ sartorius_scale/serial_type_c_usb.py
@@ -1,14 +1,41 @@
-import matplotlib.pyplot as plt
+import csv
 import numpy as np
-import serial
-  
-ser = serial.Serial("COM3", 
-                     115200, 
-                     timeout = 3, 
-                     bytesize=serial.EIGHTBITS, 
-                     parity=serial.PARITY_ODD,
-                     stopbits=serial.STOPBITS_ONE)
+import serial 
 
-#read from display
-data = ser.read(100)
-print(data)
+tare = b'\x1BU'
+print_screen = b'\x1BP'
+zero = b'\x1Bf3_'
+sound = b'\x1BQ'
+
+def connect():
+    print('connecting to device')
+    try:
+        ser = serial.Serial(
+                            port='COM3',\
+                            baudrate=115200,\
+                            parity=serial.PARITY_NONE,\
+                            stopbits=serial.STOPBITS_ONE,\
+                            bytesize=serial.EIGHTBITS,\
+                            timeout=4)
+        print("connected to: " + ser.portstr)
+    
+    except serial.serialutil.SerialException:
+        print("The port is in use. Unplug USB-C")  
+    return ser
+  
+def serial_send(parameter,ser):
+    try:
+        ser.write(parameter)
+        print("Serial Send %s" %parameter)
+    except NameError as e:
+        print('[Error]Connect to scale')
+
+def main():
+    ser = connect()
+    serial_send(sound,ser)
+    ser.close() 
+
+if __name__ == "__main__":
+     main()
+
+
