dc753ce862b15a485f05bd78a350a30e5183c906
noahmazza <noah.mazza17@gmail.com>
2023-03-31 12:42:20 -0600

    Changed stuff for testing Mettler Toledo scale

--- sartorius_scale/data_aquisition.py
+++ sartorius_scale/data_aquisition.py
@@ -1,12 +1,19 @@
 import numpy as np
 import serial_ports_find
+import mettler_toledo_device
 
 scale = serial_ports_find.connect_scale()
 
 if scale == None:
     scale = serial_ports_find.custom_connect_scale()
 
+scale.ser.timeout = .1
+
 print(scale.ser)
 
 while(True):
     print(scale.read_screen().decode('utf-8'))
+
+# scale = mettler_toledo_device.MettlerToledoDevice(port = "COM5")
+
+# scale.get_commands()
