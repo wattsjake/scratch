c3cb2e8c4fc2020183bef64fd26ba5c21afd6cd7
noahmazza <noah.mazza17@gmail.com>
2023-04-03 11:15:01 -0600

    Testing data collection

--- sartorius_scale/data_aquisition.py
+++ sartorius_scale/data_aquisition.py
@@ -1,6 +1,11 @@
 import numpy as np
 import serial_ports_find
 import mettler_toledo_device
+import csv
+import time
+
+def update_tss(start_time):
+    return time.time() - start_time
 
 scale = serial_ports_find.connect_scale()
 
@@ -11,9 +16,14 @@ scale.ser.timeout = .1
 
 print(scale.ser)
 
+file_out = open('data.csv', 'w', newline='')
+csv_writer = csv.writer(file_out)
+start_time = time.time()
+tss = update_tss(start_time)
+
 while(True):
     print(scale.read_screen().decode('utf-8'))
+    time.sleep(1)
+    tss = update_tss(start_time)
+    csv_writer.writerow([tss, scale.read_screen().decode('utf-8')])
 
-# scale = mettler_toledo_device.MettlerToledoDevice(port = "COM5")
-
-# scale.get_commands()
