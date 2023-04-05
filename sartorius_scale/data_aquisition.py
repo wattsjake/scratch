cf088fb34ad0a8a92ce0619cb1ebb79173c2dd2c
noahmazza <noah.mazza17@gmail.com>
2023-04-05 10:24:00 -0600

    Testing code with Mettler Toledo, CSV Export

--- sartorius_scale/data_aquisition.py
+++ sartorius_scale/data_aquisition.py
@@ -3,6 +3,8 @@ import serial_ports_find
 import mettler_toledo_device
 import csv
 import time
+import data_class
+import keyboard
 
 def update_tss(start_time):
     return time.time() - start_time
@@ -15,15 +17,22 @@ if scale == None:
 scale.ser.timeout = .1
 
 print(scale.ser)
+collector = data_class.DataCollect(scale=scale, unit='g', time_unit='s', delay=0.1, delay_overall=True)
 
-file_out = open('data.csv', 'w', newline='')
-csv_writer = csv.writer(file_out)
-start_time = time.time()
-tss = update_tss(start_time)
+break_key = 'q'
+start_key = 's'
+stop_key = 'e'
 
 while(True):
-    print(scale.read_screen().decode('utf-8'))
-    time.sleep(1)
-    tss = update_tss(start_time)
-    csv_writer.writerow([tss, scale.read_screen().decode('utf-8')])
+    data = scale.read_screen().decode('utf-8')
+    print(data)
+    data = data.split(' ')
+    if keyboard.is_pressed(break_key):
+        break
+    if keyboard.is_pressed(start_key) and not collector.go_measure:
+        collector.StartMeasure(data[4])
+    if keyboard.is_pressed(stop_key):
+        collector.StopMeasure()
+    collector.AddMeasure(data[4])
 
+collector.ExportData('test.csv')
--- sartorius_scale/data_class.py
+++ sartorius_scale/data_class.py
@@ -7,34 +7,50 @@ class DataCollect:
         self.unit = kwargs.get('unit', 'g')
         self.time_unit = kwargs.get('time_unit', 's')
         self.delay = kwargs.get('delay', 0.5)
-        self.delay_overall = kwargs.get('delay_type', False)
+        self.delay_overall = kwargs.get('delay_type', True)
         self.measures = []
         self.times = []
         self.go_measure = False
+        self.start_time = None
+        self.last_time = None
 
-    def StartMeasure(self):
+    def StartMeasure(self, measurement):
         self.go_measure = True
-        self.times.append(0)
-        self.measures.append(self.scale.read_screen().decode('utf-8'))
-        self.start_time = time.time()
+        if self.start_time == None:
+            self.start_time = time.time()
+        self.times.append(time.time() - self.start_time)
+        self.measures.append(measurement)
         self.last_time = self.start_time
+        
 
     def StopMeasure(self):
         self.go_measure = False
 
-    def GetMeasure(self, **kwargs):
+    def AddMeasure(self, measurement, **kwargs):
 
+        if not self.go_measure:
+            return
+        
         # Get the time increment. If delay_overall is true, get the overall delay based on the number of measures.
         # Otherwise, get the delay since the last measure.
-        time_increment = None
-        if(self.delay_overall):
-            time_increment = (time.time() - self.start_time) / len(self.measures)
-        else:
-            time_increment = time.time() - self.last_time
+        time_increment = self.GetTimeIncrement()
 
-        # If you have the flag to measure and the delay has been met, get the next measure
-        if (self.go_measure and (time.time() - self.last_time < self.delay)) or kwargs.get('force', False):
-            self.measures.append(self.scale.read_screen().decode('utf-8'))
+        # If the delay has been met or a measure is being forced, add measure.
+        if (time_increment > self.delay) or kwargs.get('force', False):
             self.times.append(time.time() - self.start_time)
+            self.measures.append(measurement)
+            self.last_time = time.time()
 
-    
+    def GetTimeIncrement(self, **kwargs):
+        time_increment = 0
+        if( (self.delay_overall or kwargs.get('overall', False)) and len(self.measures) > 0):
+            return (time.time() - self.start_time) / len(self.measures)
+        else:
+            return time.time() - self.last_time
+
+    def ExportData(self, file_out, **kwargs):
+        with open(file_out, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['Time (' + self.time_unit + ')', 'Weight (' + self.unit + ')'])
+            for i in range(len(self.measures)):
+                writer.writerow([self.times[i], self.measures[i]])
