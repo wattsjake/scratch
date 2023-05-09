42026438aaa4fef8328bd19cd4534c3b962b856b
noahmazza <noah.mazza17@gmail.com>
2023-05-09 10:57:56 -0600

    Added automatic measuring functionality; Bugfixed

--- sartorius_scale/data_class.py
+++ sartorius_scale/data_class.py
@@ -3,7 +3,7 @@ import csv
 import copy
 from dataclasses import dataclass
 import numpy as np
-from numpy_da import DynamicArray
+from scale_superclass import Scale
 
 
 @dataclass
@@ -25,6 +25,20 @@ class DataCollect:
         self.start_times = []  # Start times for each column
         self.indexes = []  # Indexes for each column
         self.column = 0  # Which column data is being collected in
+        self.scale = kwargs.get('scale', None)
+        self.final_measure = []  # Whether or not the final measure has been taken
+        self.__initialize_columns()
+        
+
+    def set_scale(self, scale: Scale):
+        """Sets the scale for the data collector.
+
+        Args:
+            scale (Scale): The scale to be used for data collection.
+        """        
+
+        self.scale = scale
+
 
     def StartMeasure(self, measurement: Data):
         """Start measuring data.
@@ -54,7 +68,8 @@ class DataCollect:
 
         self.go_measure = False
 
-    def AddMeasure(self, measurement: Data, **kwargs):
+
+    def AddMeasure(self, *args, **kwargs):
         """Adds a measure to the current column of the data collector.
 
         Adds a measure to the current column of the data collector if the delay has been met
@@ -66,8 +81,7 @@ class DataCollect:
 
         # Add measure if the measure is being forced
         if kwargs.get('force', False):
-            self.__add_measure(measurement)
-            return
+            self.__add_measure(*args)
 
         # Do not add the measure if measurements are not being taken
         if (not self.go_measure):
@@ -79,23 +93,46 @@ class DataCollect:
 
         # If the delay has been met, add measure.
         if (time_increment > self.delay):
-            self.__add_measure(measurement)
+            self.__add_measure(*args)
 
 
-    def __add_measure(self, measurement: Data):
+    def __add_measure(self, *args, **kwargs):
         """Adds a measure to the current column of the data collector.
 
         Private method to be used within data collection class for measures that must be added.
-        Creates a new column if the current column is not initialized.
 
         Args:
             measurement (data_class.Data): The measurement to be added.
         """        
 
-        # If the column is not initialized, initialize it
-        if( len(self.measures) <= self.column):
-            self.measures.append([None for y in range(0)])
-        self.measures[self.column].append(copy.deepcopy(measurement))
+        column = kwargs.get('column', self.column)
+        
+        if(self.final_measure[column] == True):
+            try:
+                self.measures[column].append(args[0])
+            except IndexError:
+                self.measures[column].append(self.scale.get_weight_data())
+            return
+        
+        # If the last measure in the column is not the final measure, update it and say it's the final measure
+        self.__update_measure(*args, **kwargs)
+        self.final_measure = True
+        
+
+    def __update_measure(self, *args, **kwargs):
+        """Updates a measure in the current column of the data collector.
+
+        Private method to be used within data collection class for measures that must be updated.
+        """        
+
+        column = kwargs.get('column', self.column)
+        row = kwargs.get('row', -1)
+
+        try:
+            self.measures[column][row] = args[0]
+        except IndexError:
+            self.measures[column][row] = self.scale.get_weight_data()
+
 
     def GetTimeIncrement(self, **kwargs):
         time_increment = 0
@@ -103,6 +140,7 @@ class DataCollect:
             return (time.time() - self.start_times[self.column]) / len(self.measures[self.column])
         else:
             return time.time() - self.measures[self.column][-1].time
+
 
     def ExportData(self, file_out, **kwargs):
         with open(file_out, 'w', newline='') as csvfile:
@@ -118,7 +156,7 @@ class DataCollect:
                     fields['Time (' + self.measures[column][0].time_unit + ') ' + str(column)] = None
                 fields['Measure (' + self.measures[column][0].unit + ') ' + str(column)] = None
 
-            # Create dictwriter object and write header
+            # Create dictwriter object and write the header
             writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fields)
             writer.writeheader()
 
@@ -137,11 +175,26 @@ class DataCollect:
                         row['Measure (' + self.measures[column][0].unit + ') ' + str(column)] = self.measures[column][i].measure
                 writer.writerow(row)
 
-    def ChangeColumn(self, column):
+    def ChangeColumn(self, column, **kwargs):
+
+        # If forced, initialize all columns up to the specified column
+        if kwargs.get('force', False):
+            self.column = column
+            self.__initialize_columns()
+            return
+
+        # If not forced and the column does not exist, raise an error
+        if column >= len(self.measures):
+            raise IndexError("Column does not exist")
         self.column = column
-        self.StopMeasure()
 
     def NextColumn(self):
-        if len(self.measures) < self.column + 1:
-            self.measures = np.append(self.measures, np.ndarray((1, 20), dtype=Data))
-        self.ChangeColumn(self.column + 1)
+        self.ChangeColumn(self.column + 1, force = True)
+
+    def __initialize_columns(self, **kwargs):
+
+        # If the column is not initialized, initialize it and all previous uninitialized columns
+        while(len(self.measures) <= kwargs.get('column', self.column)):
+            self.measures.append([None for y in range(0)])
+            self.final_measure.append(True)
+        
