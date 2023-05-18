666563e66b5a255f18425a02ca8a2b513a8d1e48
noahmazza <noah.mazza17@gmail.com>
2023-05-18 10:39:55 -0600

    Removed extraneous code

--- sartorius_scale/scaledrivers/scale.py
+++ sartorius_scale/scaledrivers/scale.py
@@ -75,12 +75,6 @@ class Scale:
         self.ser.write(command)
         return self.ser.readline()
 
-
-
-    # All of this was generated with GitHub Copilot.  We'll see if it works.
-
-
-
     def __enter__(self):
         return self
 
@@ -98,59 +92,3 @@ class Scale:
 
     def sound(self):
         return self.send_receive(self.SOUND)
-
-    def get_info(self):
-        self.ser.write(b'V\r')
-        return self.ser.readline()
-
-    def get_stability(self):
-        self.ser.write(b'S\r')
-        return self.ser.readline()
-
-    def get_units(self):
-        self.ser.write(b'U\r')
-        return self.ser.readline()
-
-    def get_mass(self):
-        self.ser.write(b'P\r')
-        return self.ser.readline()
-
-    def get_status(self):
-        self.ser.write(b'ST\r')
-        return self.ser.readline()
-
-    def get_error(self):
-        self.ser.write(b'ER\r')
-        return self.ser.readline()
-
-    def get_battery(self):
-        self.ser.write(b'BT\r')
-        return self.ser.readline()
-
-    def get_temperature(self):
-        self.ser.write(b'TM\r')
-        return self.ser.readline()
-
-    def get_calibration(self):
-        self.ser.write(b'CL\r')
-        return self.ser.readline()
-
-    def get_calibration_date(self):
-        self.ser.write(b'CD\r')
-        return self.ser.readline()
-
-    def get_calibration_time(self):
-        self.ser.write(b'CT\r')
-        return self.ser.readline()
-
-    def get_calibration_user(self):
-        self.ser.write(b'CU\r')
-        return self.ser.readline()
-
-    def get_calibration_weight(self):
-        self.ser.write(b'CW\r')
-        return self.ser.readline()
-
-    def get_calibration_weight_units(self):
-        self.ser.write(b'CU\r')
-        return self.ser.readline()
