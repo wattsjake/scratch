7f3eb2e016ebc33d381eaec3faf1581bf29b9b2c
noahmazza <noah.mazza17@gmail.com>
2023-06-05 08:08:19 -0600

    MettlerToledo has less info to initialize

--- sartorius_scale/scaledrivers/mettlertoledo.py
+++ sartorius_scale/scaledrivers/mettlertoledo.py
@@ -6,26 +6,24 @@ from data_class import Data
 
 class MettlerToledo(Scale):
 
+    # All possible settings for options required to initiate connection listed in case a brute force connection is necessary
+    # Default configuration is 19200 baud, 8 data bits, 1 stop bit, odd parity
+    BAUDRATES = (600, 1200, 2400, 4800, 9600, 19200)  # Tuple of possible baud-rates
+    BYTESIZES = (serial.EIGHTBITS, serial.SEVENBITS)  # Tuple of possible byte sizes
+    STOPBITS = (serial.STOPBITS_ONE, serial.STOPBITS_TWO)  # Tuple of possible stop bits
+    PARITIES = (serial.PARITY_NONE, serial.PARITY_ODD, serial.PARITY_EVEN)  # Tuple of possible parities
+
+    DEFAULT = {"baudrate": 9600, "bytesize": serial.EIGHTBITS, "stopbits": serial.STOPBITS_ONE, "parity": serial.PARITY_NONE}  # Tuple of Default configuration
+
+    COMMAND_START = ''
+    COMMAND_END = '\r\n'
+    TARE = 'T'
+    PRINT_SCREEN = 'SI'
+    ZERO = 'Z'
+    SOUND = 'M12'
+
     def __init__(self, port):
-
-        # All possible settings for options required to initiate connection listed in case a brute force connection is necessary
-        # Default configuration is 19200 baud, 8 data bits, 1 stop bit, odd parity
-        self.BAUDRATES = (600, 1200, 2400, 4800, 9600, 19200)  # Tuple of possible baud-rates
-        self.BYTESIZES = (serial.EIGHTBITS, serial.SEVENBITS)  # Tuple of possible byte sizes
-        self.STOPBITS = (serial.STOPBITS_ONE, serial.STOPBITS_TWO)  # Tuple of possible stop bits
-        self.PARITIES = (serial.PARITY_NONE, serial.PARITY_ODD, serial.PARITY_EVEN)  # Tuple of possible parities
-
-        self.DEFAULT = {"baudrate": 9600, "bytesize": serial.EIGHTBITS, "stopbits": serial.STOPBITS_ONE, "parity": serial.PARITY_NONE}  # Tuple of Default configuration
-
-        self.COMMAND_START = ''
-        self.COMMAND_END = '\r\n'
-        self.TARE = 'T'
-        self.PRINT_SCREEN = 'SI'
-        self.ZERO = 'Z'
-        self.SOUND = 'M12'
-        
         self.set_serial(port, self.DEFAULT)
-        
     
     def get_weight_data(self):
         weight_string = self.send_receive(self.PRINT_SCREEN)
