9d7f96bb1fc14cd0343a948423c4ecbc6b44337b
noahmazza <noah.mazza17@gmail.com>
2023-06-06 10:08:08 -0600

    Tolerance now stored as percent

--- sartorius_scale/weigh_tolerance.py
+++ sartorius_scale/weigh_tolerance.py
@@ -127,12 +127,10 @@ while True:
                 sg.popup_error("Please enter a valid target measurement.")
             else:
                 if values['-TOLERANCE-IS-PERCENT-']:
-                    tolerance = (fn.try_float(values['-TOLERANCE-']) / 100) * target_measure.measure
+                    tolerance = fn.try_float(values['-TOLERANCE-']) / 100
                 else:
-                    tolerance = fn.try_float(values['-TOLERANCE-'])
+                    tolerance = fn.try_float(values['-TOLERANCE-']) / target_measure.measure
                 window.next_layout()
-
-            
 
     # Tolerance measure events
     if window.get_layout() == 2:
@@ -140,11 +138,11 @@ while True:
         if event == '-TARE-':
             scale1.tare()
         if measurement.stable:
-            if abs(measurement.measure - target_measure.measure) <= tolerance:
+            if abs(measurement.measure - target_measure.measure) <= tolerance * target_measure.measure:
                 window['-INSTRUCTION-2-'].update("Measurement is within tolerance. Please remove the item from the scale.")
             else:
                 amount_text = ""
-                if abs(measurement.measure - target_measure.measure) <= tolerance * 1.5:
+                if abs(measurement.measure - target_measure.measure) <= tolerance * 1.5 * target_measure.measure:
                     amount_text = "a little "
                 direction_text = "Add "
                 if measurement > target_measure:
