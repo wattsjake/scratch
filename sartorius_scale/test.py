5f2d8dc7763064290ffd3d29d4da6399c83cd96a
noahmazza <noah.mazza17@gmail.com>
2023-06-20 10:44:25 -0600

    Added aio to testing file

--- sartorius_scale/test.py
+++ sartorius_scale/test.py
@@ -1,7 +1,12 @@
 from scaledrivers import scale, mettlertoledo
+import asyncio
 
-scale = mettlertoledo.MettlerToledo("COM5")
-cin = input()
-while cin != 'q':
-    print(scale.send_receive(cin))
+async def main():
+    scale = mettlertoledo.MettlerToledo("COM5")
     cin = input()
+    while cin != 'q':
+        print(await scale.send_receive(cin))
+        cin = input()
+
+if __name__ == "__main__":
+    asyncio.run(main())
