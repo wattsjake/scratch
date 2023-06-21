1b241db321f95268175234acd22227c7cd375443
noahmazza <noah.mazza17@gmail.com>
2023-06-21 09:39:02 -0600

    Added command for checking MT response completion

--- sartorius_scale/scaledrivers/mettlertoledo.py
+++ sartorius_scale/scaledrivers/mettlertoledo.py
@@ -33,6 +33,9 @@ class MettlerToledo(scale.Scale):
             weight_data.stable = True
         return weight_data
 
+    def response_complete(self, next_line):
+        return not " B " in next_line
+
     def __str__(self):
         return "Mettler Toledo"
 
