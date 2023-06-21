611e13d0d4688c7c31d0b1aebc3d735aeb078048
noahmazza <noah.mazza17@gmail.com>
2023-06-21 11:29:22 -0600

    New method for sending and printing response

--- sartorius_scale/test.py
+++ sartorius_scale/test.py
@@ -1,11 +1,15 @@
 from scaledrivers import scale, mettlertoledo
 import asyncio
 
+async def print_send_receive(scale: scale.Scale, command: str):
+    cin = await scale.send_receive(command)
+    print(cin)
+
 async def main():
     scale = mettlertoledo.MettlerToledo("COM5")
     cin = input()
     while cin != 'q':
-        print(await scale.send_receive(cin))
+        await print_send_receive(scale, cin)
         cin = input()
 
 if __name__ == "__main__":
